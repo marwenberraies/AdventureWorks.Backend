181729b333202237ca79a3914244c8b66345f3d0
enriqueescobar-askida <enrique.escobar@askida.com>
2019-02-04 16:30:03 -0500

    upate

--- AdventureWorks.Oltp.2012/ConsoleApp/Program.cs
+++ AdventureWorks.Oltp.2012/ConsoleApp/Program.cs
@@ -10,6 +10,50 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            /*
+            using (NorthwindDbContext db = new NorthwindDbContext())
+            {
+                var data = db.Categories;
+                foreach (Category item in data)
+                {
+                    Console.WriteLine(item.CategoryId + " " + item.CategoryName);
+                }
+                Console.WriteLine();
+
+                var history = db.ProcedureCustomerOrderHistory("ALFKI");
+                foreach (ProcedureCustomerOrderHistoryReturnModel item in history)
+                {
+                    Console.WriteLine("{0, 2} {1}", item.Total, item.ProductName);
+                }
+
+                Console.WriteLine();
+
+                OrderDetail orderDetail = db.OrderDetails.First();
+                Console.WriteLine(orderDetail.Product.ProductName);
+                Console.WriteLine();
+                var csvToInt = db.ProcedureCsvToInt("2,4,6,8").Select(x => x.IntValue);
+                foreach (var item in csvToInt)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine();
+                Engineer engineer = new Engineer
+                {
+                    Forename = "Simon",
+                    Surname = "Hughes"
+                };
+                db.Engineers.Add(engineer);
+                db.SaveChanges();
+                EngineerOrder order = new EngineerOrder
+                {
+                    OrderPlaced = DateTime.Now,
+                    Engineer = engineer
+                };
+                db.EngineerOrders.Add(order);
+                db.SaveChanges();
+                Console.ReadLine();
+            }
+             * /
         }
     }
 }
